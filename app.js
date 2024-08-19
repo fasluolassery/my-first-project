47cb8e6dd6288bfa2136697a1ff1a4b6373dfe4a
faslurahman5577 <fasluolassery@gmail.com>
2024-08-19 20:19:54 +0530

    mongo time out 30

--- app.js
+++ app.js
@@ -12,9 +12,13 @@ require('dotenv').config();
 // -----------------------------------------------
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("Connected to Mongo DB"))
-.catch(( error ) => console.log("Error connecting to Mongo DB", error));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+  })
+  .then(() => console.log("Connected to Mongo DB"))
+  .catch((error) => console.log("Error connecting to Mongo DB", error));
 // ---------------------------------------------------------------------
 
 const app = express();
